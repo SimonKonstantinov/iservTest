4f854abf49a2f1efe97736303c203643f7063751
Simon Konstantinov <Sim-PC@yandex.ru>
2018-01-31 13:39:27 +0300

    сегодня очистил  принятую  информацию от  грязи, завтра  надо уже парсить xml и выводить инфу. Все проще чем кажется

--- test/Controlers/NancyFXModule.cs
+++ test/Controlers/NancyFXModule.cs
@@ -2,32 +2,21 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using Nancy;
 using Nancy.ModelBinding;
 
 namespace nancyfx.Modules
-{
-    class Dinosaur
-    {
-        public string Name { get; set; }
 
-      
-    }
+{
     public class NancyFXModule : NancyModule
     {
-     
-
-        private static List<Dinosaur> dinosaurs = new List<Dinosaur>()
-    {
-    
-    };
-        
 
         public NancyFXModule()
         {
             Get["/Upload"] = param => View["Upload.html"];
-            Get["/dinosaurs/{id}"] = parameters => dinosaurs[parameters.id - 1];
+
             Post["/Upload"] = param =>
 
             {
@@ -36,10 +25,19 @@ namespace nancyfx.Modules
                 var length = this.Request.Body.Length;
                 var data = new byte[length];
                 id.Read(data, 0, (int)length);
-                var body = System.Text.Encoding.Default.GetString(data);
+                string body = System.Text.Encoding.Default.GetString(data);
+                //   var xml = body.IndexOf('<');
+                body = body.Substring(body.IndexOf('<'));
+                body = body.Remove(body.LastIndexOf('>')+1);
+
+                //string pattern = @"^< ";
+                //string target = " ";
+                //body = body.Replace("------WebKitFormBoundarySH6HZJ5OdTcNVrdb Content - Disposition: form - data; name = 'file'; filename = '2017-12-12_13-06-36_0300_34744732.xml'", "");
+                //Regex regex = new Regex(@"^<", RegexOptions.Multiline );
+                //string result = regex.Replace(body,"");
 
 
-                return 200;
+                return Response.AsXml(body);
 
 
             };
@@ -47,4 +45,3 @@ namespace nancyfx.Modules
         }
     }
 }
-
