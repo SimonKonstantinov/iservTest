e9de53fca5bcc012eca031e7147a76d2f10005d6
Simon Konstantinov <Sim-PC@yandex.ru>
2018-02-02 15:22:19 +0300

    сделать представление

--- Xml/Program.cs
+++ Xml/Program.cs
@@ -15,54 +15,20 @@ namespace Xml
             xDoc.Load(@"C:\Users\skons\Source\Repos\iservTest\test\Files\data.xml");
             // получим корневой элемент
             XmlElement xRoot = xDoc.DocumentElement;
-            // обход всех узлов в корневом элементе
-            foreach (XmlNode xnode in xRoot)
-            {
-                // получаем атрибут name
-                if (xRoot.HasChildNodes == true)
-                {
-                    if(  xnode.Name== "forms")
-                    {
-                        Console.WriteLine("fff: {0}", xnode.InnerText);
-                        foreach (XmlNode childForms in xnode.ChildNodes)
-                        {
-                            if (childForms.Name == "form")
-                            {
-                                foreach (XmlNode childForm in childForms.ChildNodes)
-                                {
-                                    if (childForm.Name == "data")
-                                    {
+    
 
-                                    }
-                                }
+            
+            //      XmlNode dareceived_datete = xRoot.SelectSingleNode("//data/received-date");
+            Console.WriteLine(xRoot.SelectSingleNode("//user/@id").Value);
+            Console.WriteLine(xRoot.SelectSingleNode("//service/@id").Value);
+            Console.WriteLine(xRoot.SelectSingleNode("//service").InnerText);
+            Console.WriteLine(xRoot.SelectSingleNode("//date").InnerText);
+            Console.WriteLine(xRoot.SelectSingleNode("//data/received-date").InnerText);
+            Console.WriteLine(xRoot.SelectSingleNode("//okato").InnerText);
 
-                            }
+            Console.WriteLine(xRoot.SelectSingleNode("//rates/rate").InnerText);
 
-                        }
 
-                    }
-
-                }
-                // обходим все дочерние узлы элемента user
-              
-            }
-            foreach (XmlNode childnode in xRoot.ChildNodes)
-            {
-                // если узел - company
-                if (childnode.Name == "date")
-                {
-                    Console.WriteLine("fff: {0}", childnode.InnerText);
-                }
-                // если узел age
-                if (childnode.Name == "received-date")
-                {
-                    Console.WriteLine("Возраст: {0}", childnode.InnerText);
-                }
-                if (childnode.Name == "okato")
-                {
-                    Console.WriteLine("Возраст: {0}", childnode);
-                }
-            }
         }
     }
 }
